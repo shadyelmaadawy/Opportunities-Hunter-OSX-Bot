f5b7fa12bc567774c9309ba1575146a1bf54d851
Shady El-Maadawy <shadudiix@192.168.1.2>
2023-12-30 03:52:00 +0200

    Upload ML model and checker

---
+++ MLChecker/MLChecker/ViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  MLChecker
+//
+//  Created by Shady El-Maadawy on 30/12/2023.
+//
+
+import Cocoa
+import CoreML
+class ViewController: NSViewController {
+
+    @IBOutlet var jobTextField: NSTextView!
+    @IBOutlet weak var resultTextField: NSTextField!
+    
+    // MARK: - This Model is in early early stage, and will not be accurate with One-Hundred Percent.
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+    @IBAction func checkClick(_ sender: Any) {
+        
+        guard let jobBuffer = jobTextField.textStorage?.string else {
+            return
+        }
+        
+        let jobModel = try? AreYouIOSDev.init(
+            configuration: .init()
+        )
+        guard let jobModel = jobModel else {
+            return
+        }
+        
+        let jobInput = AreYouIOSDevInput(text: jobBuffer)
+        
+        guard let checkResult = try? jobModel.prediction(input: jobInput) else {
+            return
+        }
+
+        resultTextField.stringValue = "Are you IOS Developer? \(checkResult.label)"
+        if(checkResult.label == "Yes") {
+            resultTextField.textColor = .systemBlue
+        } else {
+            resultTextField.textColor = .systemRed
+        }
+        
+    }
+}
+
